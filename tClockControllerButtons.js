3e87ef6b5a4e879cd0904e92435e387cd55696ff
AlesiaKolbik <32718331+AlesiaKolbik@users.noreply.github.com>
2018-07-26 12:00:50 +0300

    ++

---
+++ tClockControllerButtons.js
@@ -0,0 +1,22 @@
+define(function() {
+    class TClockControllerButtons {
+
+        constructor(model, view) {
+            this.view = view;
+            let self = this;
+            model.changeCallback = function () {
+                self.renderView();
+            }
+            view.onCheckedCallback = function (isClicked) {
+                model.start(isClicked);
+            }
+        }
+
+        renderView() {
+            this.view.render();
+        }
+    }
+
+return TClockControllerButtons ;
+
+});
